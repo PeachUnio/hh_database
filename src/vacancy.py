0931548f84f2ef9171e717ade3dda18440f803e5
Арина Новгородова <arrrishanov@gmail.com>
2025-11-02 20:16:16 +0300

    add class Vacancy

---
+++ src/vacancy.py
@@ -0,0 +1,13 @@
+class Vacancy:
+    """Класс для работы с вакансиями"""
+    def __init__(self, vacancy_data, employer_id):
+        self.id = vacancy_data.get("id")
+        self.name = vacancy_data.get("name", "")
+        self.employer_id = employer_id
+        self.salary_from = self._get_salary(vacancy_data.get("salary"), "from")
+        self.salary_to = self._get_salary(vacancy_data.get("salary"), "to")
+        self.currency = self._get_currency(vacancy_data.get("salary"))
+        self.url = vacancy_data.get("alternate_url", "")
+        self.requirement = vacancy_data.get("snippet", {}).get("requirement", "")
+        self.experience = vacancy_data.get("experience", {}).get("name", "")
+
